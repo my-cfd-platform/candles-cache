48c3f6eac69b868f7331c4b98758747a83c4bfd7
amigin <amigin@gmail.com>
2023-03-19 15:44:55 +0200

    BidOrAsk now has from_is_bid

--- src/abstractions.rs
+++ src/abstractions.rs
@@ -3,3 +3,13 @@ pub enum BidOrAsk {
     Bid,
     Ask,
 }
+
+impl BidOrAsk {
+    pub fn from_is_bid(is_bid: bool) -> Self {
+        if is_bid {
+            Self::Bid
+        } else {
+            Self::Ask
+        }
+    }
+}
