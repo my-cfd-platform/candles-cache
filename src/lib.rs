ea4e02c1e237df40079433fb55b527ad11cfccdf
amigin <amigin@gmail.com>
2023-03-19 13:55:45 +0200

    Candle to persist

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 mod abstractions;
 mod candle_date_cache;
 mod candle_date_key;
-mod candle_persist_cache;
+
 mod candles_cache_data;
 mod candles_instrument_cache;
 mod models;
 mod settings;
 
 pub use abstractions::*;
-pub use candle_persist_cache::*;
+
 pub use candles_cache_data::*;
 pub use candles_instrument_cache::*;
 pub use settings::*;
